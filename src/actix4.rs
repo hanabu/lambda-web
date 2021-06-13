fc238b84fe63e5677ec5874ceec06b8faf761769
Hanabusa Masahiro <masa@hanabusa.net>
2021-06-13 23:42:39 +0900

    Fix doc comment

--- src/actix4.rs
+++ src/actix4.rs
@@ -17,26 +17,30 @@ use lamedh_runtime::{
     Handler as LambdaHandler,
 };
 
-/// Run Actix-web application on AWS Lambda
+/// Run Actix web application on AWS Lambda
 ///
 /// ```
-/// #[get("/{id}/{name}/index.html")]
-/// async fn index(path: web::Path<(u32, String)>) -> impl Responder {
-///     let (id, name) = path.into_inner();
-///     format!("Hello {}! id:{}", name, id)
+/// use lambda_web::actix_web::{self, get, App, HttpServer, Responder};
+/// use lambda_web::{is_running_on_lambda, run_actix_on_lambda, LambdaError};
+///
+/// #[get("/")]
+/// async fn hello() -> impl Responder {
+///     format!("Hello")
 /// }
 ///
 /// #[actix_web::main]
 /// async fn main() -> Result<(),LambdaError> {
 ///     let factory = move || {
-///         App::new().service(index)
+///         App::new().service(hello)
 ///     };
 ///     if is_running_on_lambda() {
-///         run_actix_on_lambda(factory).await?
+///         // Run on AWS Lambda
+///         run_actix_on_lambda(factory).await?;
 ///     } else {
 ///         // Run local server
 ///         HttpServer::new(factory).bind("127.0.0.1:8080")?.run().await?;
 ///     }
+///     Ok(())
 /// }
 /// ```
 ///
